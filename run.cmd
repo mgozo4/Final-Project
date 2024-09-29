335b6a400312ab490df06d7b5306e9d372e4b36f
kimkihwan94 <king08kgh@naver.com>
2024-09-29 20:03:48 +0900

    Initial commit on man branch

---
+++ run.cmd
@@ -0,0 +1,1 @@
+javadoc -docletpath target\yogi_project-0.0.1-SNAPSHOT-jar-with-dependencies.jar -doclet com.example.yogi_project.Doclet %*
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javadoc -docletpath target/yogi_project-0.0.1-SNAPSHOT-jar-with-dependencies.jar -doclet com.example.yogi_project.Doclet "$@"
